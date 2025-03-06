a140718f9bf3a6fdbaab65a6467bce40eac1c569
LegendaryM <49125466easongo@gmail.com>
2025-03-06 15:17:05 +0800

    更新 .DS_Store 檔案

--- python-learning/pratice/ex031.py
+++ python-learning/pratice/ex031.py
@@ -6,9 +6,13 @@ a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
 
-if a**2 + b**2 == c**2:
+if a + b <= c or a + c <= b or b + c <= a:
+    print(f"{a}, {b}, {c} 三邊形成的三角形不成立")
+elif a**2 + b**2 == c**2:
     print(f"{a}, {b}, {c} 三邊形成的三角形是直角三角形")
 elif a**2 + b**2 > c**2:
     print(f"{a}, {b}, {c} 三邊形成的三角形是銳角三角形")
-else:
+elif a**2 + b**2 < c**2:
     print(f"{a}, {b}, {c} 三邊形成的三角形是鈍角三角形")
+else:
+    print(f"{a}, {b}, {c} 三邊形成的三角形不成立")
